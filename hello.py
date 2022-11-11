a8280d3d908cc60bee8cebcaabd0dc2a1eb567a3
AbdifatahIbrahim <cfataxyare5716@gmail.com>
2022-11-11 21:24:22 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+print ('Hello World!')
+# creating a list
+names = ['mohamed', 'jama', 'nour', 'mouse', 'hussein']
+# looping through the names and printing them
+for name in names:
+    print(f"{name.title()}", end=' ')
