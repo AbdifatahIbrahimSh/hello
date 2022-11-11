7015b91ce24da5078f184d13adb8124c3e1680ea
AbdifatahIbrahim <cfataxyare5716@gmail.com>
2022-11-11 21:26:04 +0300

    new condition check

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ names = ['mohamed', 'jama', 'nour', 'mouse', 'hussein']
 # looping through the names and printing them
 for name in names:
     print(f"{name.title()}", end=' ')
+
+
+if  5 > 4: 
+    print (5)   
